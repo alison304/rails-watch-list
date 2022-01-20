83c86f0ac1edbd7f6487bd0a6144fb78fd666aff
Alison Paico <alison.jpq@gmail.com>
2022-01-20 17:06:46 -0500

    av

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,15 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all
   end
 
   def show
+    @lists = List.find(params[:id])
+    @bookmarks = Bookmark.new
   end
 
   def new
+    @list = List.new
   end
 
   def create
