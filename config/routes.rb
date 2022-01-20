4e08017063f279a83561fd0d11b799a17e13f681
Alison Paico <alison.jpq@gmail.com>
2022-01-20 16:47:37 -0500

    av

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create, :delete]
+  end  
 end
