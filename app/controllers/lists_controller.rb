a80b86ef59f9704d95da70ccf56551c7d255ee85
Alison Paico <alison.jpq@gmail.com>
2022-01-21 13:07:20 -0500

    av

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,13 @@
 class ListsController < ApplicationController
+before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    @lists = List.find(params[:id])
-    @bookmarks = Bookmark.new
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
   end
 
   def new
@@ -13,8 +15,26 @@ class ListsController < ApplicationController
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
-  def delete
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
-  get 'bookmarks/delete'
+  # get 'bookmarks/new'
+  # get 'bookmarks/create'
+  # get 'bookmarks/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
   resources :lists, except: [:edit, :update] do
     resources :bookmarks, only: [:new, :create, :delete]
-  end  
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
